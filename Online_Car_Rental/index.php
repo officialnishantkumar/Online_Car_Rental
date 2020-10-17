01defa72a029d2f967d3d36fcffa0fcf6057c34a
officialnishantkumar <72223697+officialnishantkumar@users.noreply.github.com>
2020-10-17 01:55:29 -0700

    Update index.php

--- Online_Car_Rental/index.php
+++ Online_Car_Rental/index.php
@@ -70,7 +70,7 @@ error_reporting(0);
     <div class="section-header text-center">
       <h2>Find the Best <span>CarForYou</span></h2>
       <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
-            <p> We provide so many Offer</p>
+            <p> We provide so many Offer like discounts in products</p>
           </div>
     <div class="row"> 
       
